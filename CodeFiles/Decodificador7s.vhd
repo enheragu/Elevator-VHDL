c462354ce99748593fbbd67b77c171d5fb523935
AlbertoBeneit <alberbeneit@gmail.com>
2016-12-29 14:03:04 +0100

    Modifico comentarios

--- CodeFiles/Decodificador7s.vhd
+++ CodeFiles/Decodificador7s.vhd
@@ -16,8 +16,8 @@ begin
 		PisoVoy7s <=  "1001111" WHEN "0001", -- Lectura Piso 1
 					     "0010010" WHEN "0010", -- Lectura Piso 2
 					     "0000110" WHEN "0100", -- Lectura Piso 3
-			           "1001100" WHEN "1000", -- Lectura Piso 4
-			           "0110000" WHEN others; -- Error cuando la lectura de los sensores no hace referencia a nigun piso
+			                     "1001100" WHEN "1000", -- Lectura Piso 4
+			                     "0110000" WHEN others; -- Error 
  
 end Behavioral;
 
