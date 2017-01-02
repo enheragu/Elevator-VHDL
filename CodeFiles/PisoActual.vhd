6fd8088dcd2d261692e62cfa13573e40284d895e
AranchaCM <aranchacanelo@gmail.com>
2017-01-02 16:55:59 +0100

    Modificado Bloque PisoActual

--- CodeFiles/PisoActual.vhd
+++ CodeFiles/PisoActual.vhd
@@ -1,30 +1,31 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity PisoActual is
-    Port ( sensorEstoy : in  STD_LOGIC_VECTOR (3 downto 0);
-           pisoEstoy : out  STD_LOGIC_VECTOR (3 downto 0)
+    Port ( SensorEstoy : in  STD_LOGIC_VECTOR (3 downto 0);
+           PisoEstoy : out  STD_LOGIC_VECTOR (3 downto 0)
 			 );
 end entity PisoActual;
 
 architecture Dataflow of PisoActual is
 
-signal aux1: STD_LOGIC_VECTOR (sensorEstoy'range);
+signal aux1: STD_LOGIC_VECTOR (SensorEstoy'range) := (others => '0');
 
 begin
+	process (SensorEstoy)
+	begin
+		IF (sensorEstoy /= "0000") then
+		aux1 <= sensorEstoy;
+		end if;
+	end process;
 
-IF (sensorEstoy /= "0000") then
-	aux1 <= sensorEstoy;
-
-with sensorEstoy select
+	with sensorEstoy select
 		pisoEstoy <= aux1 when "0000",
-				 sensorEstoy when "0001",
-				 sensorEstoy when "0010",
-				 sensorEstoy when "0100",
-				 sensorEstoy when "1000",
-				 "1111" when others;
+			     sensorEstoy when "0001",
+			     sensorEstoy when "0010",
+			     sensorEstoy when "0100",
+			     sensorEstoy when "1000",
+			     "1111" when others;
 		 
 end architecture Dataflow;
 
