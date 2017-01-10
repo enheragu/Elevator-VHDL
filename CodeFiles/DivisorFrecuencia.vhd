270d654ddcfbd62c1e48bf5bd4cb43a67932b769
AlbertoBeneit <alberbeneit@gmail.com>
2017-01-10 20:22:24 +0100

    Divisor de frecuencia archivos

---
+++ CodeFiles/DivisorFrecuencia.vhd
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity DivisorFrecuencia is
+	Port(
+	     PisoVoy7s : in  STD_LOGIC; -- reloj entrada
+        reset     : in  STD_LOGIC;
+        PisoVoy    : out STD_LOGIC
+	);
+end DivisorFrecuencia;
+
+architecture Behavioral of DivisorFrecuencia is
+		signal temporal: STD_LOGIC; --temporal: es el reloj de SALIDA
+		signal contador: integer range 0 to 124999 := 0; --escala: ESCALA/2
+begin
+		divisor_frecuencia: process (reset, PisoVoy7s) 
+		begin
+			if (reset = '1') then -- reset: ponemos todo a cero
+					temporal <= '0';
+					contador <= 0;
+			elsif rising_edge(PisoVoy7s) then
+					if (contador = 124999) then
+						temporal <= NOT(temporal); -- realizamos cambio
+						contador <= 0;
+					else
+						contador <= contador+1;
+					end if;
+        end if;
+      end process;  
+PisoVoy <= temporal; -- SABEMOS QUE TEMPORAL ES LA SALIDA
+end Behavioral;
+
---
+++ CodeFiles/DivisorFrecuencia_tb.vhd
@@ -0,0 +1,51 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ENTITY DivisorFrecuencia_tb IS
+END DivisorFrecuencia_tb;
+ 
+ARCHITECTURE behavior OF DivisorFrecuencia_tb IS 
+    -- Component Declaration for the Unit Under Test (UUT)
+    COMPONENT DivisorFrecuencia PORT(
+			  PisoVoy7s : in  STD_LOGIC; -- reloj entrada
+			  reset     : in  STD_LOGIC;
+			  PisoVoy   : out STD_LOGIC
+        );
+    END COMPONENT;
+	-- SEÑALES
+	-- Entradas
+    signal PisoVoy7s : std_logic := '0'; -- asignamos valor inicial
+    signal reset   : std_logic := '0';
+    -- Salidas
+    signal PisoVoy  : std_logic;
+	 
+   -- appropriate port name 
+   constant clock_period : time := 20 ns; -- frecuencia reloj entrada
+	
+BEGIN
+	-- Instantiate the Unit Under Test (UUT)
+   uut: DivisorFrecuencia PORT MAP (
+			PisoVoy7s => PisoVoy7s,
+			reset   => reset,
+			PisoVoy  => PisoVoy
+        );
+		  
+   -- Clock process definitions AQUI ESTABLECEMOS EL RELOJ DE ENTRADA
+   clock_process :process
+   begin
+		PisoVoy7s <= '0';
+		wait for clock_period/2;
+		PisoVoy7s <= '1';
+		wait for clock_period/2;
+   end process;
+	
+   -- Stimulus process
+   stim_proc: process
+   begin		
+		  reset <= '1'; -- Condiciones iniciales.
+        wait for 500 ns;
+        reset <= '0'; -- ¡A trabajar!
+        wait; 
+   end process;
+
+END;
