f637520a1825b6f9aa79d7377dbaa5e39dc5ccea
AlbertoBeneit <albertobeneitbenita@MacBook-Pro-de-Alberto-2.local>
2017-01-11 17:45:12 +0100

    Divisor Frecuencia

--- CodeFiles/DivisorFrecuencia.vhd
+++ CodeFiles/DivisorFrecuencia.vhd
@@ -14,16 +14,16 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity DivisorFrecuencia is
-    Port ( ControlMotor7s  : in  STD_LOGIC_VECTOR(7 downto 0);
-           ControlPuerta7s : in  STD_LOGIC_VECTOR(7 downto 0);
-           PisoVoy7s       : in  STD_LOGIC_VECTOR(7 downto 0);
-           PisoEstoy7s     : in  STD_LOGIC_VECTOR(7 downto 0);
+    Port ( ControlMotor7s  : in  STD_LOGIC_VECTOR(6 downto 0);
+           ControlPuerta7s : in  STD_LOGIC_VECTOR(6 downto 0);
+           PisoVoy7s       : in  STD_LOGIC_VECTOR(6 downto 0);
+           PisoEstoy7s     : in  STD_LOGIC_VECTOR(6 downto 0);
            Clk             : in  STD_LOGIC; 
            A0              : out  STD_LOGIC;
            A1              : out  STD_LOGIC;
            A2              : out  STD_LOGIC;
            A3              : out  STD_LOGIC;
-		   Salida7s        : out  STD_LOGIC_VECTOR(7 downto 0);	
+		   Salida7s        : out  STD_LOGIC_VECTOR(6 downto 0)
 	);
 end DivisorFrecuencia;
 
@@ -34,9 +34,9 @@ architecture Behavioral of DivisorFrecuencia is
 		signal SigA1       : std_logic;
 		signal SigA2       : std_logic;
 		signal SigA3       : std_logic;
-		signal SigSalida7s : std_logic_vector(7 downto 0);
+		signal SigSalida7s : std_logic_vector(6 downto 0);
 begin
-	process (reset, Clk) 
+	process (Clk) 
 	begin
 		if rising_edge(Clk) then
 			if (contador = 16) then
@@ -52,7 +52,7 @@ begin
           		SigSalida7s <= ControlMotor7s;
 			end if;
 			if (contador >= 4 and contador <8) then 
-				SigA1<='0' ; SigA1<='1'; SigA2<='0'; SigA3<='0';
+				SigA0<='0' ; SigA1<='1'; SigA2<='0'; SigA3<='0';
 				SigSalida7s <= ControlPuerta7s;
 			end if;
 			if (contador >= 8 and contador <12) then 
--- CodeFiles/DivisorFrecuencia_tb.vhd
+++ CodeFiles/DivisorFrecuencia_tb.vhd
@@ -1,51 +1,103 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
+
 ENTITY DivisorFrecuencia_tb IS
 END DivisorFrecuencia_tb;
  
 ARCHITECTURE behavior OF DivisorFrecuencia_tb IS 
     -- Component Declaration for the Unit Under Test (UUT)
-    COMPONENT DivisorFrecuencia PORT(
-			  PisoVoy7s : in  STD_LOGIC; -- reloj entrada
-			  reset     : in  STD_LOGIC;
-			  PisoVoy   : out STD_LOGIC
+    COMPONENT DivisorFrecuencia
+    PORT(
+         ControlMotor7s  : IN  std_logic_vector(6 downto 0);
+         ControlPuerta7s : IN  std_logic_vector(6 downto 0);
+         PisoVoy7s       : IN  std_logic_vector(6 downto 0);
+         PisoEstoy7s     : IN  std_logic_vector(6 downto 0);
+         Clk             : IN  std_logic;
+         A0              : OUT  std_logic;
+         A1              : OUT  std_logic;
+         A2              : OUT  std_logic;
+         A3              : OUT  std_logic;
+         Salida7s        : OUT  std_logic_vector(6 downto 0)
         );
     END COMPONENT;
-	-- SEÃ‘ALES
-	-- Entradas
-    signal PisoVoy7s : std_logic := '0'; -- asignamos valor inicial
-    signal reset   : std_logic := '0';
-    -- Salidas
-    signal PisoVoy  : std_logic;
-	 
-   -- appropriate port name 
-   constant clock_period : time := 20 ns; -- frecuencia reloj entrada
+	-- SEÑALES
+   --Inputs
+   signal ControlMotor7s  : std_logic_vector(6 downto 0) := (others => '0');
+   signal ControlPuerta7s : std_logic_vector(6 downto 0) := (others => '0');
+   signal PisoVoy7s        : std_logic_vector(6 downto 0) := (others => '0');
+   signal PisoEstoy7s     : std_logic_vector(6 downto 0) := (others => '0');
+   signal Clk             : std_logic := '0';
+ 	--Outputs
+   signal A0              : std_logic;
+   signal A1              : std_logic;
+   signal A2              : std_logic;
+   signal A3              : std_logic;
+   signal Salida7s        : std_logic_vector(6 downto 0);
+   -- Clock period definitions
+   constant Clk_period : time := 10 ns;
 	
+	-- Vector de pruebas
+	TYPE vtest is record
+	 A0              : std_logic;
+    A1              : std_logic;
+    A2              : std_logic;
+    A3              : std_logic;
+    Salida7s        : std_logic_vector(6 downto 0);
+   END RECORD;
+   TYPE vtest_vector IS ARRAY (natural RANGE <>) OF vtest;
+	CONSTANT test: vtest_vector := (
+		(A0=> '1', A1=> '0', A2=> '0', A3=> '0',Salida7s => "1111111"),
+		(A0=> '0', A1=> '1', A2=> '0', A3=> '0',Salida7s => "1110000"),
+		(A0=> '0', A1=> '0', A2=> '1', A3=> '0',Salida7s => "0001111"),
+		(A0=> '0', A1=> '0', A2=> '0', A3=> '1',Salida7s => "0000000")
+		 );
+
 BEGIN
 	-- Instantiate the Unit Under Test (UUT)
    uut: DivisorFrecuencia PORT MAP (
-			PisoVoy7s => PisoVoy7s,
-			reset   => reset,
-			PisoVoy  => PisoVoy
+          ControlMotor7s => ControlMotor7s,
+          ControlPuerta7s => ControlPuerta7s,
+          PisoVoy7s => PisoVoy7s,
+          PisoEstoy7s => PisoEstoy7s,
+          Clk => Clk,
+          A0 => A0,
+          A1 => A1,
+          A2 => A2,
+          A3 => A3,
+          Salida7s => Salida7s
         );
-		  
-   -- Clock process definitions AQUI ESTABLECEMOS EL RELOJ DE ENTRADA
-   clock_process :process
+   -- Clock process definitions
+   Clk_process :process
    begin
-		PisoVoy7s <= '0';
-		wait for clock_period/2;
-		PisoVoy7s <= '1';
-		wait for clock_period/2;
+		Clk <= '0';
+		wait for Clk_period/2;
+		Clk <= '1';
+		wait for Clk_period/2;
    end process;
-	
    -- Stimulus process
    stim_proc: process
    begin		
-		  reset <= '1'; -- Condiciones iniciales.
-        wait for 500 ns;
-        reset <= '0'; 
-        wait; 
+		FOR i IN 0 TO test'HIGH LOOP  
+				ControlMotor7s <= "1111111";
+				ControlPuerta7s <= "1110000";
+				PisoVoy7s <= "0001111";
+				PisoEstoy7s <= "0000000";
+				WAIT FOR 40 ns;
+					ASSERT A0 = test(i).A0 and 
+							 A1 = test(i).A1 and
+							 A2 = test(i).A2 and
+							 A3 = test(i).A3 and 
+							 Salida7s = test(i).Salida7s 
+
+							 REPORT "Salida incorrecta." 
+						SEVERITY FAILURE;
+			END LOOP;
+		
+			ASSERT false
+			REPORT "Simulacion finalizada. Test superado." 
+			SEVERITY FAILURE;
+
+      
    end process;
 
 END;
